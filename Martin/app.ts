50d7d8fbf92382997c4c805ac1446694a06524ca
Predrag <martin.durisin@gmail.com>
2021-11-29 18:52:38 +0100

    update create-password.ts route and adding sendPassword view

--- Martin/app.ts
+++ Martin/app.ts
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 import userRouter from './routes/user';
 import emailRouter from './routes/email';
 import basicAuth from './lib/auth/auth';
-import createPassword from './routes/create-password';
+import createPassword, { generatedPassword } from './routes/create-password';
 import requestAuth from './lib/auth/require-auth';
 import findUserByCredentials from './lib/dao/find-user';
 
@@ -14,10 +14,14 @@ app.set('view engine', 'pug');
 const port = 3000;
 const jsonParser = bodyParser.json();
 
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 app.use(compress());
-app.use('/login', jsonParser, createPassword);
+app.use('/login', jsonParser, createPassword, (req, res) => { res.render('sendPassword', { title: 'Password', password: generatedPassword }); });
 app.use(requestAuth);
 app.use(basicAuth(findUserByCredentials));
 app.use('/emails', emailRouter);
