0535983a80eaced666f6eaf260a954f75b640640
Predrag <martin.durisin@gmail.com>
2021-12-11 23:40:54 +0100

    refactoring API

--- api/controllers/user.controller.ts
+++ api/controllers/user.controller.ts
@@ -1,35 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
-import mongoose from 'mongoose';
 import { findUsers, insertNewUser } from '../services/user.service';
 import { UsersInterface } from '../interfaces/users.interface';
 
-export async function getUsers(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  const { query } = req;
-  try {
-    await findUsers({ query }).then((users) => {
-      res.status(200).json(users);
-    });
-  } catch (err) {
-    next(err);
-  }
+export async function getUsers(req: Request, res: Response, next: NextFunction) {
+	const { query } = req;
+	try {
+		await findUsers({ query }).then((users) => {
+			res.status(200).json(users);
+		});
+	} catch (err) {
+		next(err);
+	}
+	next();
 }
 
-export async function postCreateNewUser(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  const user: UsersInterface = req.body;
-  try {
-    mongoose.set('debug', true);
-    await insertNewUser(user).then((insertUser) => {
-      res.status(200).json(insertUser);
-    });
-  } catch (err) {
-    next(err);
-  }
+export async function postCreateNewUser(req: Request, res: Response, next: NextFunction) {
+	const user: UsersInterface = req.body;
+	try {
+		await insertNewUser(user).then((insertUser) => {
+			res.status(200).json(insertUser);
+		});
+	} catch (err) {
+		next(err);
+	}
 }
--- api/services/user.service.ts
+++ api/services/user.service.ts
@@ -6,68 +6,75 @@ import UserModel from '../models/user.model/user.model';
 import userBuilder from '../models/user.model/user.builder';
 
 export async function findUsers(userId: any) {
-  const user = userBuilder(userId);
-  let userFound;
-  let isEmpty;
-  let validProperties = true;
-  let invalidProperty;
-  let setLimit = 3;
-  try {
-    if (Object.prototype.hasOwnProperty.call(userId.query, 'limit')) {
-      if (parseInt(userId.query.limit, 10) > 4) {
-        return { message: `Max limit is set to ${setLimit}` };
-      }
-      setLimit = parseInt(userId.query.limit, 10);
-    }
-    Object.keys(userId.query).forEach((key) => {
-      if (Object.prototype.hasOwnProperty.call(user, key) !== true) {
-        validProperties = false;
-        invalidProperty = key;
-        throw new Error();
-      }
-    });
-    await connectToDatabase();
-    Object.keys(user).forEach((key) => { if (user[key] === undefined || user[key] === '') { delete user[key]; } });
-    if (Object.prototype.hasOwnProperty.call(user, 'id') && validProperties) {
-      if (mongoose.Types.ObjectId.isValid(user.id)) {
-        userFound = await UserModel.find({ _id: new ObjectId(user.id) });
-        isEmpty = Object.keys(userFound).length === 0;
-      }
-      if (isEmpty !== true && isEmpty !== undefined) {
-        return userFound;
-      } return { message: 'You have entered incorrect or not existing ID' };
-    }
-    userFound = await UserModel.find(user).limit(setLimit);
-    isEmpty = Object.keys(userFound).length === 0;
-    if (isEmpty !== true && validProperties) {
-      return userFound;
-    }
-    return { message: 'User not found' };
-  } catch (err) {
-    if (err instanceof Error) {
-      return { message: `You have entered incorrect parameter: ${invalidProperty}` };
-    }
-    return err;
-  }
+	const user = userBuilder(userId);
+	let userFound;
+	let isEmpty;
+	let validProperties = true;
+	let setLimit = 50;
+	try {
+		if (Object.prototype.hasOwnProperty.call(userId.query, 'limit')) {
+			if (parseInt(userId.query.limit, 10) > setLimit) {
+				throw new Error(`Max limit is set to ${setLimit}`);
+			}
+			setLimit = parseInt(userId.query.limit, 10);
+		}
+		Object.keys(userId.query).forEach((key) => {
+			if (Object.prototype.hasOwnProperty.call(user, key) !== true) {
+				validProperties = false;
+				throw new Error(`You have entered incorrect parameter: ${key}`);
+			}
+		});
+		await connectToDatabase();
+		Object.keys(user).forEach((key) => {
+			if (user[key] === undefined || user[key] === '') {
+				delete user[key];
+			}
+		});
+		if (Object.prototype.hasOwnProperty.call(user, 'id') && validProperties) {
+			if (mongoose.Types.ObjectId.isValid(user.id)) {
+				userFound = await UserModel.find({
+					_id: new ObjectId(user.id),
+				});
+				isEmpty = Object.keys(userFound).length === 0;
+			}
+			if (isEmpty !== true && isEmpty !== undefined) {
+				return userFound;
+			}
+			throw new Error('You have entered incorrect or not existing ID');
+		}
+		userFound = await UserModel.find(user).limit(setLimit);
+		isEmpty = Object.keys(userFound).length === 0;
+		if (isEmpty !== true && validProperties) {
+			return userFound;
+		}
+		throw new Error('User not found');
+	} catch (err) {
+		if (err instanceof Error) {
+			return {
+				message: err.message,
+			};
+		}
+		return err;
+	}
 }
 
 export async function insertNewUser(user: UsersInterface) {
-  try {
-    await connectToDatabase();
-    const newUser = new UserModel({
-      username: user.username,
-      surname: user.surname,
-      name: user.name,
-      password: user.password,
-      authenticated: user.authenticated,
-    });
-    const error = newUser.validateSync();
-    if (error) {
-      return { message: error.message };
-    }
-    await newUser.save();
-    return { User: newUser, message: `${newUser.username} was inserted` };
-  } catch (err) {
-    return { message: err };
-  }
+	try {
+		await connectToDatabase();
+		const newUser = new UserModel({
+			username: user.username,
+			surname: user.surname,
+			name: user.name,
+			password: user.password,
+			authenticated: user.authenticated,
+		});
+		const error = newUser.validateSync();
+		if (error) {
+			return { message: error.message };
+		}
+		await newUser.save();
+		return { User: newUser, message: `${newUser.username} was inserted` };
+	} catch (err) {
+		return { message: err };
+	}
 }
